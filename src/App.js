a858dd4aecf9c848fd1fd13fde96f34efa961b7f
Agraj Yadav <agrajy10@gmail.com>
2021-12-23 16:47:23 +0530

    created top grid area

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ function App() {
   return (
     <div className='max-w-7xl mx-auto px-3'>
       <Header />
-      <h1>Windbnd</h1>
+
+        <main className='py-4 md:py-14'>
+
+          <div className='flex items-center justify-between gap-4 mb-9'>
+            <h1 className='text-lg md:text-2xl font-bold text-zinc-800'>Stays in Finland</h1>
+            <span className='text-sm font-medium text-zinc-600'>+12 stays</span>
+          </div>
+
+        </main>
+
       <Footer />
     </div>
   );
