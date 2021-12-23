22f54e3f837cb40a4c469a6b6aa5891dd3319a5c
Agraj Yadav <agrajy10@gmail.com>
2021-12-23 17:59:01 +0530

    changed classes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
-        <footer className="py-6 text-center text-sm font-medium text-gray-500">
-            <small>created by <strong>agraj</strong> - devChallenges.io</small>
+        <footer className="py-6 text-center font-medium text-gray-500">
+            <small className="text-sm">created by <strong>agraj</strong> - devChallenges.io</small>
         </footer>
     )
 }
