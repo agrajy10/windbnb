82816a32da23f3a5457caa7832a4d840a7cc211a
Agraj Yadav <agrajy10@gmail.com>
2021-12-24 10:00:47 +0530

    added current filter counts to search button

--- src/components/SearchDrawerBtn.jsx
+++ src/components/SearchDrawerBtn.jsx
@@ -2,14 +2,30 @@ import { useContext } from 'react';
 import AppContext from '../context/context';
 
 function SearchDrawerBtn() {
-  const { dispatch } = useContext(AppContext);
+  const { city, country, adults, children, dispatch } = useContext(AppContext);
 
   return (
     <button
       onClick={() => dispatch({ type: 'OPEN_MODAL' })}
-      className="bg-white rounded-2xl text-sm text-center leading-none font-medium shadow w-full max-w-xs sm:mx-0 mx-auto flex items-center justify-start">
-      <span className="text-zinc-400 flex-1 border-r border-r-gray-100 py-4">Add location</span>
-      <span className="text-zinc-400 flex-1 border-r border-r-gray-100 py-4">Add guests</span>
+      className="bg-white rounded-2xl text-sm text-center leading-none font-medium shadow w-full max-w-[22rem] sm:mx-0 mx-auto flex items-center justify-start">
+      {city && country ? (
+        <span className="text-zinc-800 flex-1 border-r border-r-gray-100 py-4 px-2">
+          {`${city}, ${country}`}
+        </span>
+      ) : (
+        <span className="text-zinc-400 flex-1 border-r border-r-gray-100 py-4 px-2">
+          Add location
+        </span>
+      )}
+      {adults + children ? (
+        <span className="text-zinc-800 flex-1 border-r border-r-gray-100 py-4 px-2">
+          {adults + children}
+        </span>
+      ) : (
+        <span className="text-zinc-400 flex-1 border-r border-r-gray-100 py-4 px-2">
+          Add guests
+        </span>
+      )}
       <span className="w-14 flex-shrink-0">
         <svg
           xmlns="http://www.w3.org/2000/svg"
