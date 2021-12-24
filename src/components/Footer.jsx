b02ccb40d59c3c4533ecf50813af1e0cb013e10b
Agraj Yadav <agrajy10@gmail.com>
2021-12-24 13:50:41 +0530

    username correction in footeR

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="py-6 text-center font-medium text-gray-500">
       <small className="text-sm">
-        created by <strong>agraj</strong> - devChallenges.io
+        created by <strong>agrajy10</strong> - devChallenges.io
       </small>
     </footer>
   );
